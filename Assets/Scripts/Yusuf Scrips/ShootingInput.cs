f32efa55e3f7fb381d06a9ba6cc29bfa8ec01158
YusufCanBektas <yusufcan.bektas@hotmail.com>
2026-09-23 15:34:01 +0200

    update Flaschenabschiessen und optionspanal

--- Assets/Scripts/Yusuf Scrips/ShootingInput.cs
+++ Assets/Scripts/Yusuf Scrips/ShootingInput.cs
@@ -3,8 +3,35 @@ using UnityEngine;
 public class ShootingInput : MonoBehaviour
 {
     [Header("Referenzen")]
-    [Tooltip("Die Kamera, aus der geschossen wird. Falls leer gelassen, wird automatisch die Kamera auf diesem GameObject verwendet")]
+    [Tooltip("Die Kamera, aus der geschossen wird. Falls leer, wird automatisch die Kamera auf diesem GameObject verwendet")]
     [SerializeField] private Transform cameraTransform;
+
+    [Header("Einstellungen")]
+    [Tooltip("Maximale Schussreichweite")]
+    [SerializeField] private float shootRange = 500f;
+
+    [Tooltip("Layer, auf dem die Flaschen liegen")]
+    [SerializeField] private LayerMask bottleLayer;
+
+    [Header("Munition")]
+    [Tooltip("Wie viele Schuesse der Spieler pro Runde hat")]
+    [SerializeField] private int maxAmmo = 8;
+
+    [Header("Sichtbare Kugel")]
+    [Tooltip("Kleines Kugel-Prefab, das beim Schuss sichtbar zum Ziel fliegt")]
+    [SerializeField] private GameObject bulletPrefab;
+
+    [Tooltip("Startpunkt der Kugel, z.B. die Muendung der Pistole. Falls leer, wird die Kamera-Position genutzt")]
+    [SerializeField] private Transform muzzlePoint;
+
+    private bool shootingEnabled = false;
+    private int currentAmmo;
+
+    /// <summary>
+    /// Aktuell verbleibende Schuesse - kann von der UI ausgelesen werden.
+    /// </summary>
+    public int CurrentAmmo => currentAmmo;
+    public int MaxAmmo => maxAmmo;
 
     private void Awake()
     {
@@ -14,30 +41,22 @@ public class ShootingInput : MonoBehaviour
         }
     }
 
-    [Header("Einstellungen")]
-    [Tooltip("Maximale Schussreichweite")]
-    [SerializeField] private float shootRange = 50f;
-
-    [Tooltip("Layer, auf dem die Flaschen liegen (im Inspector auswaehlen)")]
-    [SerializeField] private LayerMask bottleLayer;
-
-    [Header("Sichtbare Kugel")]
-    [Tooltip("Kleines Kugel-Prefab (z.B. eine Sphere), das beim Schuss sichtbar zum Ziel fliegt")]
-    [SerializeField] private GameObject bulletPrefab;
-
-    [Tooltip("Startpunkt der Kugel, z.B. die Muendung der Pistole. Falls leer, wird die Kamera-Position genutzt")]
-    [SerializeField] private Transform muzzlePoint;
-
-    private bool shootingEnabled = false;
-
     private void Update()
     {
         if (!shootingEnabled) return;
 
-        // Linke Maustaste = schiessen
         if (Input.GetButtonDown("Fire1"))
         {
-            TryShoot();
+            if (currentAmmo > 0)
+            {
+                currentAmmo--;
+                TryShoot();
+            }
+            else
+            {
+                Debug.Log("Keine Munition mehr!");
+                // Hier koennte spaeter ein "Klick"-Sound fuer leere Waffe rein
+            }
         }
     }
 
@@ -57,8 +76,6 @@ public class ShootingInput : MonoBehaviour
         }
         else
         {
-            // Kein Treffer = einfach daneben geschossen. Die Kugel fliegt trotzdem
-            // sichtbar geradeaus bis zur maximalen Reichweite, statt einfach zu fehlen.
             bulletTarget = ray.origin + ray.direction * shootRange;
         }
 
@@ -84,11 +101,16 @@ public class ShootingInput : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Wird von BottleShootingGame beim Rundenstart aufgerufen.
+    /// Setzt die Munition auf den vollen Wert zurueck.
+    /// </summary>
     public void EnableShooting()
     {
         shootingEnabled = true;
+        currentAmmo = maxAmmo;
     }
-    
+
     public void DisableShooting()
     {
         shootingEnabled = false;
